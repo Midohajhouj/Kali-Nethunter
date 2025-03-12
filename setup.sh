0b1085b4c221d9fa188d2000530f5f776372f3c6
MIDO <155222995+Midohajhouj@users.noreply.github.com>
2025-03-11 23:11:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,10 +25,23 @@ log() {
     
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
-
-
-package() {
-    banner
+banner() {
+    clear
+    printf "${blue}##################################################\n"
+    printf "${blue}##                                              ##\n"
+    printf "${blue}##  88      a8P         db        88        88  ##\n"
+    printf "${blue}##  88    .88'         d88b       88        88  ##\n"
+    printf "${blue}##  88   88'          d8''8b      88        88  ##\n"
+    printf "${blue}##  88 d88           d8'  '8b     88        88  ##\n"
+    printf "${blue}##  8888'88.        d8YaaaaY8b    88        88  ##\n"
+    printf "${blue}##  88P   Y8b      d8''''''''8b   88        88  ##\n"
+    printf "${blue}##  88     '88.   d8'        '8b  88        88  ##\n"
+    printf "${blue}##  88       Y8b d8'          '8b 888888888 88  ##\n"
+    printf "${blue}##        Minimal and Powerful Solution         ##\n"
+    printf "${blue}############## Coded by LIONMAD ##################${reset}\n\n"
+    printf "\e[1;32m### Let's get started the installation! ###\e[0m\n"
+    printf "\n"
+}
     log "Updating and upgrading packages..."
     echo -e "${R} [${W}-${R}]${C} Updating and upgrading packages...${W}"
     
